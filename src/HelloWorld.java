d47d17b30403d17f90f382678e74b1e378443ceb
Sandra Rimke <bruklytesan@gmail.com>
2022-11-11 21:17:22 +0200

    Comments, Variables, Declaration, Initialization

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello Sandra");
+        System.out.println("Hello Sandra"); // Printing my name
+        // This is comment
+        /*
+        Multi-line
+        comment
+         */
+
+
     }
 }
